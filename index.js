2624928ba9af09b8b933061aa86649559627977a
Jayden Nicholson <jdnick27@gmail.com>
2024-07-07 20:48:23 -0500

    tweaks to the post message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const CronJob = require("cron").CronJob;
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
-const { download } = require("./utilities");
+const { download, readFileToArray, getRandomInt } = require("./utilities");
 const { fetchCardById } = require("./fetchPokemonData");
 
 app.listen(port, () => {
@@ -12,19 +12,30 @@ app.listen(port, () => {
 });
 
 const tweet = async () => {
-    const cardId = 'ex10-1'; // Example card ID
+    const setsfilePath = './pokemon_card_sets.txt';
+    const linesArray = readFileToArray(setsfilePath);
+    const cardId = `${linesArray[await getRandomInt(0, 99)]}-${await getRandomInt(1, 5)}`; // Example card ID
     const card = await fetchCardById(cardId);
     const cardName = card.name;
     const imageURL = card.images.large;
     const filename = `images/${cardName}.png`;
+    let cardType = '';
+    if (card.types.length > 1) {
+        cardType = card.types[0];
+        for (let i = 1; i < card.types.length; i++) {
+            cardType = ', ' + card.types[i];
+        }
+    } else {
+        cardType = card.types[0];
+    }
 
     download(imageURL, filename, async function () {
         try {
             const mediaId = await twitterClient.v1.uploadMedia(`./${filename}`);
-            
+
 
             await twitterClient.v2.tweet({
-                text: `Hello world! This is an image ${card.name}`,
+                text: `Daily #Pokemon Cards\n\n${card.name}\nType: ${cardType}\nSeries: ${card.set.series}\nRelease Date: ${card.set.releaseDate}\nTrend Price: $${card.cardmarket.prices.trendPrice}`,
                 media: {
                     media_ids: [mediaId]
                 }
@@ -35,7 +46,7 @@ const tweet = async () => {
     });
 };
 
-const cronTweet = new CronJob("*/30 * * * * *", async () => {
+const cronTweet = new CronJob("0 */6 * * *", async () => {
     tweet();
 });
 
--- utilities.js
+++ utilities.js
@@ -7,4 +7,22 @@ const download = function (uri, filename, callback) {
   });
 };
 
-module.exports = { download };
+// Function to read a text file and convert to array
+function readFileToArray(filePath) {
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    const lines = data.split('\n').map(line => line.trim());
+    return lines;
+  } catch (err) {
+    console.error(`Error reading file ${filePath}:`, err);
+    return [];
+  }
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);     // Round up to the nearest whole number
+  max = Math.floor(max);    // Round down to the nearest whole number
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+module.exports = { download, readFileToArray, getRandomInt };
